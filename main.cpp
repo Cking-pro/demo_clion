c54d50c90b9ad1c6bdce02ad87107ab4cc43281a
CKing-pro <juzi1378@gmail.com>
2023-05-01 19:20:29 +0800

    测试clion && git

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+/**
+ *
+ * @return
+ */
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
